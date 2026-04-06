e6eb1b2f59595996fb139f4fa1bb0236056aef75
Trinity Ndlovu <ndlovutrinity18@gmail.com>
2026-04-06 20:52:36 +0200

    Added JavaScript

---
+++ script.js
@@ -0,0 +1,6 @@
+let submitButton = document.getElementById("submit-button");
+function handleSubmit(event) {
+    event.preventDefault();
+    alert("This form is still under development. Please Use the email provided below to contact me. Thank you for your understanding.");
+}
+submitButton.addEventListener("click", handleSubmit);
